b44a63a695ef019ccc9817cf4357dab9e876aad4
Dawid Pereira <pereiradawid@outlook.com>
2025-07-21 19:52:08 +0200

    refactor: make formula description version-agnostic
    - Remove (Alpha) from description for future automation
    - Description will remain accurate for all release types
    - Prepares formula for automated updates

--- Formula/quetty.rb
+++ Formula/quetty.rb
@@ -1,5 +1,5 @@
 class Quetty < Formula
-  desc "Terminal-based Azure Service Bus queue manager (Alpha)"
+  desc "Terminal-based Azure Service Bus queue manager"
   homepage "https://github.com/dawidpereira/quetty"
   url "https://github.com/dawidpereira/quetty/archive/refs/tags/v0.1.0-alpha.1.tar.gz"
   sha256 "c041d4dd9404a41e1d5b3f289d393bda454ce592604674839bfcff3eafaa5c2d"
